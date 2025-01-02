43d656d7ebf33b7860d612b3b75363f7242624b9
ramprasathmk <mkramprasath12@gmail.com>
2025-01-02 12:19:55 +0545

    update file
    Signed-off-by: ramprasathmk <mkramprasath12@gmail.com>

--- server.js
+++ server.js
@@ -1,33 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Post = require('./models/Post');
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3333;
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Post = require('./models/Post')
+const path = require('path')
+const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT || 3333
 
-mongoose.connect('mongodb://127.0.0.1:27017/microblog_db', { useNewUrlParser: true, useUnifiedTopology: true });
-app.set('view engine', 'ejs');
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Database Connected')
+  })
+  .catch((err) => {
+    console.log(err.message)
+  })
+
+app.set('view engine', 'ejs')
 
 // Set up middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.get('/', async (req, res) => {
-  const posts = await Post.find().sort({ timestamp: -1 });
-  res.render('./index.ejs', { posts });
-  
-});
+  const posts = await Post.find().sort({ timestamp: -1 })
+  res.render('index', { posts })
+})
 
 app.post('/create', async (req, res) => {
-  const { title, content } = req.body;
-  const newPost = new Post({ title, content });
-  await newPost.save();
-  res.redirect('/');
-});
+  const { title, content } = req.body
+  const newPost = new Post({ title, content })
+  await newPost.save()
+  res.redirect('/')
+})
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running in the http://localhost:${PORT}`);
-});
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
